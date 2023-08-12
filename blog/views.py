7a5c8ed07cb92a4fc85aa8b0b837126960d1297d
Vadim <korovin555@.gmail.com>
2023-08-12 17:32:40 +0700

    comments

--- blog/views.py
+++ blog/views.py
@@ -16,9 +16,13 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post':post})
+                  {'post':post,
+                   'comments': comments,
+                   'form':form})
 
 def post_list(request):
     post_list = Post.published.all()
@@ -70,15 +74,14 @@ def post_share(request, post_id):
 
 @require_POST
 def post_comment(request, post_id):
-    post = get_object_or_404(Post,
-                             id=post_id,
+    post = get_object_or_404(Post, id=post_id, \
                              status=Post.Status.PUBLISHED)
 
     comment = None
 
-    form = CommentForm(data=request)
+    form = CommentForm(data=request.POST)
     if form.is_valid():
-        comment =form.save(commit=False)
+        comment = form.save(commit=False)
         comment.post = post
         comment.save()
     return render(request, 'blog/post/comment.html',
